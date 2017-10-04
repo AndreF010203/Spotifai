a5567e1d3f0cfbb0e032c477df4733a1002592b0
andrea <andrea.facchini.io@gmail.com>
2017-10-04 15:49:14 +0200

    Songs ordered

--- prova.py
+++ prova.py
@@ -10,4 +10,6 @@ import matplotlib.pyplot as plt
 from numpy import *
 from scipy import *
 
-tracklist = genfromtxt('tracks_final.csv')
+tracklist = genfromtxt('tracks_final.csv', skip_header=1, usecols=(0, 3), missing_values="nan", filling_values="0")
+
+print(tracklist)
