3392730315c878ecad47b980f82132fd467365df
andrea <andrea.facchini.io@gmail.com>
2017-10-17 11:07:41 +0200

    aa

--- generate_tracks_with_tags.py
+++ generate_tracks_with_tags.py
@@ -6,16 +6,12 @@ templist=[]
 for t in tracksfinal:
     tempvar=[]
     tempvar.append(t[0])
-    tags=t[5]
-    if(tags!="[]"):
-        modtags=list(map(lambda x:int(x),tags.replace("'","").replace("[","").replace("]","").split(",")))
-        modtags+=[0]*(5-len(modtags))
-        for x in modtags:
-            tempvar.append(x)
+    tags=t[4]
+    if(tags!="[]" and tags!="[None]"):
+        tempvar=tags.replace("[","").replace("]","")
         templist.append(tempvar)
     else:
-        for x in range(5):
-            tempvar.append(0)
+        tempvar=0
         templist.append(tempvar)
 myarr=array(templist)
-save("tracks_with_tags.npy",myarr)
+save("tracks_with_album.npy",myarr)
